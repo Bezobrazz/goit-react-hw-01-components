1b727c6517e041699ca5fc98aaa84de5c74cef4e
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2023-12-30 10:02:32 +0200

    fixed profile styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,30 +2,32 @@ import s from './Profile.module.css';
 
 export default function Profile({ username, tag, location, avatar, stats }) {
   return (
-    <div className={s.card}>
-      <div className="description">
-        <div className={s.avatarWrapper}>
-          <img src={avatar} alt="User avatar" className={s.avatar} />
+    <div className={s.cardWrapper}>
+      <div className={s.card}>
+        <div className="description">
+          <div className={s.avatarWrapper}>
+            <img src={avatar} alt="User avatar" className={s.avatar} />
+          </div>
+          <h2 className={s.username}>{username}</h2>
+          <p className={s.descrTag}>@{tag}</p>
+          <p className={s.descrLoca}>{location}</p>
         </div>
-        <h2 className={s.username}>{username}</h2>
-        <p className={s.descrTag}>@{tag}</p>
-        <p className={s.descrLoca}>{location}</p>
-      </div>
 
-      <ul className={s.statsWrapper}>
-        <li className={s.statsItem}>
-          <span className="label">Followers</span>
-          <span className={s.quantity}>{stats.followers}</span>
-        </li>
-        <li className={s.statsItem}>
-          <span className="label">Views</span>
-          <span className={s.quantity}>{stats.views}</span>
-        </li>
-        <li className={s.statsItem}>
-          <span className="label">Likes</span>
-          <span className={s.quantity}>{stats.likes}</span>
-        </li>
-      </ul>
+        <ul className={s.statsWrapper}>
+          <li className={s.statsItem}>
+            <span className={s.label}>Followers</span>
+            <span className={s.quantity}>{stats.followers}</span>
+          </li>
+          <li className={s.statsItem}>
+            <span className={s.label}>Views</span>
+            <span className={s.quantity}>{stats.views}</span>
+          </li>
+          <li className={s.statsItem}>
+            <span className={s.label}>Likes</span>
+            <span className={s.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
